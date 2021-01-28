5685914596c6ae5ea6722cb9ecc221864b125c79
mvkopp <mael.avennec@hotmail.fr>
2021-01-28 15:43:47 +0100

    add

---
+++ Exam/Exam.Impl/Armstrong.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exam.Impl
+{
+    public class Armstrong
+    {
+        public bool IsArmstrong(int input)
+        {
+            int res = 0;
+            for (int i = input; i > 0; i = i / 10)
+            {
+                res += (int)Math.Pow(i % 10, 3.0);
+            }
+            
+            if (input == res) return true;
+            else return false;
+        }
+    }
+}
---
+++ Exam/Exam.Impl/FizzBuzzService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Exam.Impl
+{
+    public class FizzBuzzService
+    {
+        public string Play(int upperBound)
+        {
+            string res = "";
+            for(int i = 1; i <= upperBound; i++)
+            {
+                if (i % 3 != 0 && i % 5 != 0)
+                {
+                    res += i;
+                }
+                if (i % 3 == 0)
+                {
+                    res += "Fizz";
+                }
+                if (i % 5 == 0)
+                {
+                    res += "Buzz";
+                }
+                
+            }
+            return res;
+        }
+    }
+}
